e6a6697c5f9108f02caaf2e27cb957db4f83cbe6
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-03 09:54:23 +0100

    Count the occurence of the alphabeth letters in a string.

--- letter_counter.rb
+++ letter_counter.rb
@@ -1,3 +1,22 @@
 class LetterCounter
 
+	def count_occurence_of_letters(string)
+		letters = get_letters_in_string(string)
+
+	  counts = Hash.new(0)
+	  for letter in letters
+	    counts[letter] += 1
+	  end
+	  puts counts
+	end
+
+	def get_letters_in_string(string)
+	  string.downcase.scan(/[a-z]/)
+	end
+
+
 end
+
+letter_counter = LetterCounter.new
+letter_counter.count_occurence_of_letters("There is a lot going on in this place that if you are not observative enough, you would just be left behind.")
+letter_counter.count_occurence_of_letters("Count the ocurrences of various alphabet letters in an input string and store it in hash.")
