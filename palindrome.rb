86ded1b1a7ff6029068ce69a2a53067bf9a369dd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-04 17:50:10 +0100

    Change 'application' to 'program'.

--- palindrome.rb
+++ palindrome.rb
@@ -14,7 +14,7 @@ class Palindrome
   private
 
   def get_input
-    puts "This application checks if a word is a palindrome."
+    puts "This program checks if a word is a palindrome."
     puts "Please enter a word here or enter q/Q to quit"
     input = gets.chomp
   end
