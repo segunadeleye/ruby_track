2f85745b3690aa888d9ef343ab15f3b27418711f
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-04 18:09:40 +0100

    Define deposit() and withdraw() methods.

--- customer.rb
+++ customer.rb
@@ -10,10 +10,33 @@ class Customer
     @account_no = @@account_no
   end
 
+  def deposit(amount)
+    puts "Old balance: #{@balance}"
+    puts "Amount Deposited: #{amount}"
+    @balance += amount
+    puts "New balance: #{@balance}"
+  end
+
+  def withdraw(amount)
+    puts "Old balance: #{@balance}"
+    puts "Amount Withdrawn: #{amount}"
+    @balance -= amount
+    puts "New balance: #{@balance}"
+  end
+
 end
 
-cus = Customer.new('Oluwasegun Adeleye')
+cus = Customer.new('Oluwasegun')
 puts cus.name, cus.balance, cus.account_no
 
-cus1 = Customer.new('Oluwasegun Adeleye')
+cus1 = Customer.new('Adeleye')
 puts cus1.name, cus1.balance, cus1.account_no
+
+cus1.deposit(1000)
+cus1.deposit(2000)
+
+puts cus
+puts cus1
+
+puts cus.balance
+puts cus1.balance
