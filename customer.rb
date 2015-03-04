da0fb1bfcb894cd6064af289bfc6caa130c9558d
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-04 19:09:57 +0100

    Check if customer has sufficient funds to perform a withdrawal.

--- customer.rb
+++ customer.rb
@@ -17,9 +17,13 @@ class Customer
   end
 
   def withdraw(amount)
-    old_balance = @balance
-    @balance -= amount
-    transaction_details(old_balance, @balance)
+    if amount < @balance
+      old_balance = @balance
+      @balance -= amount
+      transaction_details(old_balance, @balance)
+    else
+      puts "You do not have sufficient funds."
+    end
   end
 
   protected
@@ -32,19 +36,19 @@ class Customer
 end
 
 cus = Customer.new('Oluwasegun')
-puts cus.name, cus.balance, cus.account_no
-
 cus1 = Customer.new('Adeleye')
+
+puts cus.name, cus.balance, cus.account_no
+puts "********************"
 puts cus1.name, cus1.balance, cus1.account_no
-
+puts "********************"
 cus.deposit(1000)
+puts "********************"
 cus1.deposit(2000)
-
-puts cus.balance
-puts cus1.balance
-
+puts "********************"
 cus.withdraw(100)
-cus1.withdraw(200)
-
+puts "********************"
+cus1.withdraw(2000000)
+puts "********************"
 puts "#{cus.name} with account number #{cus.account_no} now has an account balance of #{cus.balance}"
 puts "#{cus1.name} with account number #{cus1.account_no} now has an account balance of #{cus1.balance}"
