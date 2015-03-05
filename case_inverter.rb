0924a9954f6bfc14413f2bb611693dfcc224e34b
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-05 12:01:44 +0100

    Remove unnecessary methods.

--- case_inverter.rb
+++ case_inverter.rb
@@ -2,21 +2,13 @@ class CaseInverter
 
   def initialize(string)
     @string = string
-    invert@string
   end
 
   def to_s
     "#{@string} -> #{@string.swapcase}"
   end
 
-  def invert(string)
-    puts string
-  end
-
 end
 
-ci_1 = CaseInverter.new("hello WORLD")
-ci_2 = CaseInverter.new("iT iS a gREAT dAY tODAY.")
-
-puts ci_1
-puts ci_2
+puts CaseInverter.new("hello WORLD")
+puts CaseInverter.new("iT iS a gREAT dAY tODAY.")
