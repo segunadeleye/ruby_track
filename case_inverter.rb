e42a609361fc6656115d707d6eaf7ce4271e55de
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-05 11:40:06 +0100

    Exercise 7: Invert Case.

---
+++ case_inverter.rb
@@ -0,0 +1,22 @@
+class CaseInverter
+
+  def initialize(string)
+    @string = string
+    invert@string
+  end
+
+  def to_s
+    @string.swapcase
+  end
+
+  def invert(string)
+    puts string
+  end
+
+end
+
+ci_1 = CaseInverter.new("hello WORLD")
+ci_2 = CaseInverter.new("iT iS a gREAT dAY tODAY.")
+
+puts ci_1
+puts ci_2
