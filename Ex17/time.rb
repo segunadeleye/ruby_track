b09052e378f0274309d1df87da2221d863f0f86b
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-09 11:58:42 +0100

    Actual requirements implemented.

--- Ex17/time.rb
+++ Ex17/time.rb
@@ -1,30 +1,31 @@
-class Test
+class Time
 
-  TIME = /(\d?\d):(\d\d):(\d\d)/
+  TIME_PATTERN = /^([0-2]?[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])/
 
   def sum(a, b)
-    a = TIME.match(a)
-    b = TIME.match(b)
-
+    a = TIME_PATTERN.match(a)
+    b = TIME_PATTERN.match(b)
     if a && b
-      add_sec = Integer(a[3]) + Integer(b[3])
-      add_min = Integer(a[2]) + Integer(b[2]) + (add_sec / 60)
-      add_hrs = Integer(a[1]) + Integer(b[1]) + (add_min / 60)
-      add_day = add_hrs / 24
-
-      sec = add_sec % 60
-      mins =  add_min % 60
-      hrs = add_hrs % 24
-      day = ""
-      day = "#{add_day} day & " if add_day > 0
-      # "Days: #{day}. Hours: #{hrs}. Minutes: #{0 if mins < 10}#{mins}. Seconds:#{0 if sec < 10}#{sec}"
-      "#{day}#{hrs}:#{mins}:#{sec}"
+      total_seconds = a[3].to_i + b[3].to_i
+      total_minutes = a[2].to_i + b[2].to_i + (total_seconds / 60)
+      total_hours = a[1].to_i + b[1].to_i + (total_minutes / 60)
+      total_day = total_hours / 24
+    else
+      return "Make sure the time is valid."
     end
+
+    seconds = total_seconds % 60
+    minutes =  total_minutes % 60
+    hours = total_hours % 24
+    day = ""
+    day = "#{total_day} day & " if total_day > 0
+
+    "#{day}#{hours}:#{0 if minutes < 10}#{minutes}:#{0 if seconds < 10}#{seconds}"
   end
 
 end
 
-test = Test.new
-test.sum("23:03:59", "23:03:59")
-test.sum("3:59:59", "23:59:59")
-test.sum("3:59:59", "3:59:59")
+time = Time.new
+time.sum("123:03:59", "23:03:59")
+time.sum("24:59:59", "23:59:59")
+time.sum("3:59:59", "3:60:09")
