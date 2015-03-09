31b847ddb330a1c28c16412d714052c660a415cd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-09 14:02:42 +0100

    Use group_by() method to group by length.

--- Ex10/array.rb
+++ Ex10/array.rb
@@ -1,16 +1,7 @@
 class Array
 
-  def sort_by_type
-    hash = Hash.new()
-    each do |item|
-      key = item.to_s.length
-      if hash.has_key?(key)
-        hash[key].push(item)
-      else
-        hash[key] = []
-        hash[key].push(item)
-      end
-    end
+  def group_by_type
+    hash = group_by { |n| n.to_s.length }
     hash = hash.inject({:odd => [], :even => []}) do |a, b|
       a[:odd] << b[1] if b[0].odd?
       a[:even] << b[1] if b[0].even?
@@ -21,4 +12,4 @@ class Array
 
 end
 
-['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ'].sort_by_type
+['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ'].group_by_type
