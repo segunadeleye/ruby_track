5a4740f92538a78053004fbe47b37a2d0d0b2b1a
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-18 17:40:33 +0100

    Change method name.

--- Ex15/lib/fixnum.rb
+++ Ex15/lib/fixnum.rb
@@ -1,7 +1,7 @@
 require "prime"
 class Fixnum
 
-  def find_prime
+  def find_prime_numbers_upto
     prime_numbers = []
     (2..self).step { |n| prime_numbers << n if n.prime? }
     prime_numbers
