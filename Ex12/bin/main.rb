4feea7e2ee008a94c485baa1d3c3dad49774aac1
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-19 16:37:13 +0100

    Create display message.

--- Ex12/bin/main.rb
+++ Ex12/bin/main.rb
@@ -1,4 +1,11 @@
 require_relative "../lib/string"
 
 string = "Where is the MONEY i 232 !@#\{4+4}$%^}"
-puts string.count_characters
+count = string.count_characters
+
+puts <<-eos
+Number of lowercase characters: #{count[:lowercase]}
+Number of uppercase characters: #{count[:uppercase]}
+Number of digits: #{count[:digit]}
+Number of special characters: #{count[:special_character]}
+eos
