d0f8468576e86139a89a8b79f725ecc54b0dfdc6
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-19 15:40:22 +0100

    Initialize instance variable with a block.

--- Ex20/lib/interest.rb
+++ Ex20/lib/interest.rb
@@ -1,40 +1,25 @@
 class Interest
 
+  attr_writer :principal, :time
+
   INTEREST_RATE = 0.1
 
   def initialize(&block)
-    @block = block
+    instance_eval(&block)
   end
 
-  def difference(principal, time)
-    s_i = simple_interest(principal, time)
-    c_i = compound_interest(principal, time)
-    difference = @block.call(c_i, s_i)
-    display(principal, time, c_i, s_i, difference)
+  def difference
+    difference = "%.2f" % (compound_interest - simple_interest)
   end
 
   private
 
-  def simple_interest(principal, time)
-    simple_interest = principal * time * INTEREST_RATE
+  def simple_interest
+    simple_interest = @principal * @time * INTEREST_RATE
   end
 
-  def compound_interest(principal, time)
-    compound_interest = principal * (1 + INTEREST_RATE) ** time
-  end
-
-  def display(principal, time, c_i, s_i, difference)
-    principal = "%.2f" % principal
-    s_i = "%.2f" % s_i
-    c_i = "%.2f" % c_i
-    difference = "%.2f" % difference
-
-    puts "Pricipal Amount: #{principal}",
-         "Time: #{time} years",
-         "Interest Rate: 10%",
-         "Compound Interest: #{c_i}",
-         "Simple Interest: #{s_i}",
-         "Interest Difference: #{difference}"
+  def compound_interest
+    compound_interest = @principal * (1 + INTEREST_RATE) ** @time
   end
 
 end
