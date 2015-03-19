438cd73c7eb37452b6026c658fac94acfaa77f1a
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-19 17:15:26 +0100

    Remove variable names. Replace 'instance_eval' with 'yield self'

--- Ex20/lib/interest.rb
+++ Ex20/lib/interest.rb
@@ -4,22 +4,22 @@ class Interest
 
   INTEREST_RATE = 0.1
 
-  def initialize(&block)
-    instance_eval(&block)
+  def initialize
+    yield self
   end
 
   def difference
-    difference = "%.2f" % (compound_interest - simple_interest)
+    "%.2f" % (compound_interest - simple_interest)
   end
 
   private
 
   def simple_interest
-    simple_interest = @principal * @time * INTEREST_RATE
+    @principal * @time * INTEREST_RATE
   end
 
   def compound_interest
-    compound_interest = @principal * (1 + INTEREST_RATE) ** @time
+    @principal * (1 + INTEREST_RATE) ** @time
   end
 
 end
