7cd2a1693bbe86d05b7627a787cfe7bf8142e9cd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-20 08:38:15 +0100

    Reverse iterate method for an Array.

---
+++ reverse_iterate/bin/main.rb
@@ -0,0 +1,3 @@
+require_relative "../lib/array"
+
+[2,4,6,8].reverse_iterate { |i| print "#{i} "}
---
+++ reverse_iterate/lib/array.rb
@@ -0,0 +1,9 @@
+class Array
+
+  def reverse_iterate
+    for i in -1..length
+      yield self[i]
+    end
+  end
+
+end
