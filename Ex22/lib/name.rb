b12748b0f237e639496b8894d3c5f0d7db33dc33
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-23 06:15:43 +0100

    Create #validate.

--- Ex22/lib/name.rb
+++ Ex22/lib/name.rb
@@ -4,14 +4,18 @@ require_relative "empty_field_error"
 class Name
 
   def initialize(first_name, last_name)
-    raise EmptyFieldError, "Make sure there are no empty field." if first_name.empty? || last_name.empty?
-    raise FirstNameError, "First letter of your first name must be capital." unless capitalized?(first_name)
+    validate(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
   end
 
   def capitalized?(input)
     input.capitalize == input
+  end
+
+  def validate(first_name, last_name)
+    raise EmptyFieldError, "Make sure there are no empty field." if first_name.empty? || last_name.empty?
+    raise FirstNameError, "First letter of your first name must be capital." unless capitalized?(first_name)
   end
 
 end
