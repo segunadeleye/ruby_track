445e3b379c3ffd06c0428638a8005e23c089cc61
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-23 06:16:09 +0100

    Print input.

--- Ex22/bin/main.rb
+++ Ex22/bin/main.rb
@@ -3,9 +3,10 @@ require_relative "../lib/name"
 puts "Enter your first name..."
 first_name = gets.chomp
 
-puts "Enter your last name"
+puts "Enter your last name..."
 last_name = gets.chomp
 
 name = Name.new(first_name, last_name)
 
-puts "Valid input."
+puts "Valid input.\n\n"
+puts "Name: #{first_name} #{last_name}"
